6c85d659f15c3e8334babdfa70607f4ceb653e2a
aythamirg <aythami.r.g@hotmail.com>
2023-01-29 21:09:32 +0000

    Prueba2

--- src/test/java/org/example/MiClaseTest.java
+++ src/test/java/org/example/MiClaseTest.java
@@ -7,17 +7,66 @@ import static org.junit.jupiter.api.Assertions.*;
 class MiClaseTest {
 
     @Test
-    void dia_laboral() {
+    void dia_laboralLunes() {
 
-        assertEquals(true, MiClase.dia_laboral("Lunes"));
-        assertEquals(true, MiClase.dia_laboral("Martes"));
-        assertEquals(true, MiClase.dia_laboral("Miercoles"));
-        assertEquals(true, MiClase.dia_laboral("Jueves"));
-        assertEquals(true, MiClase.dia_laboral("Viernes"));
-        assertEquals(false, MiClase.dia_laboral("Sabado"));
-        assertEquals(false, MiClase.dia_laboral("Domingo"));
-        assertEquals(false, MiClase.dia_laboral(""));
+        MiClase clase = new MiClase();
+        boolean result = clase.dia_laboral("Lunes");
+        assertEquals(true, result);
+    }
 
+    @Test
+    void dia_laboralMartes() {
 
+        MiClase clase = new MiClase();
+        boolean result = clase.dia_laboral("Martes");
+        assertEquals(true, result);
+    }
+
+    @Test
+    void dia_laboralMiercoles() {
+
+        MiClase clase = new MiClase();
+        boolean result = clase.dia_laboral("Miercoles");
+        assertEquals(true, result);
+    }
+
+    @Test
+    void dia_laboralJueves() {
+
+        MiClase clase = new MiClase();
+        boolean result = clase.dia_laboral("Jueves");
+        assertEquals(true, result);
+    }
+
+    @Test
+    void dia_laboralViernes() {
+
+        MiClase clase = new MiClase();
+        boolean result = clase.dia_laboral("Viernes");
+        assertEquals(true, result);
+    }
+
+    @Test
+    void dia_laboralSabado() {
+
+        MiClase clase = new MiClase();
+        boolean result = clase.dia_laboral("Sabado");
+        assertEquals(false, result);
+    }
+
+    @Test
+    void dia_laboralDomingo() {
+
+        MiClase clase = new MiClase();
+        boolean result = clase.dia_laboral("Domingo");
+        assertEquals(false, result);
+    }
+
+    @Test
+    void dia_laboralSinResultado() {
+
+        MiClase clase = new MiClase();
+        boolean result = clase.dia_laboral("");
+        assertEquals(false, result);
     }
 }
